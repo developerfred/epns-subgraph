01a3acf99e1eeb6ac9570c9336f4acfa8378e7a4
codingsh <developerfred@protonmail.ch>
2020-12-05 04:50:00 +0000

    feat(EPNSProxy): add EPNSProxy contract

--- src/mapping.ts
+++ src/mapping.ts
@@ -12,6 +12,10 @@ import {
   UpdateChannel as UpdateChannelEvent,
   Withdrawal as WithdrawalEvent
 } from "../generated/EPNSCore/EPNSCore"
+import {
+  Upgraded as UpgradedEvent,
+  AdminChanged as AdminChangedEvent,
+} from "../generated/EPNSProxy/EPNSProxy"
 import { 
   AddChannel,
   DeactivateChannel,
@@ -22,7 +26,9 @@ import {
   Subscribe,
   Unsubscribe,
   UpdateChannel,
-  Withdrawal
+  Withdrawal,
+  Upgraded,
+  AdminChanged
    } from "../generated/schema"
 
 export function handleAddChannel(event: AddChannelEvent): void {
@@ -183,5 +189,24 @@ export function handleWithdrawal(event: WithdrawalEvent): void {
   entity.to = event.params.to
   entity.token = event.params.token
   entity.amount = event.params.amount
+  entity.save()
+}
+
+export function handleUpgraded(event: UpgradedEvent): void {
+  let entity = new Upgraded(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+
+  entity.implementation = event.params.implementation
+  entity.save()
+}
+
+export function handleAdminChanged (event: AdminChangedEvent ): void {
+  let entity = new AdminChanged(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+
+  entity.newAdmin = event.params.newAdmin
+  entity.previousAdmin = event.params.previousAdmin
   entity.save()
 }
